6575a4a457c26cfef82e48080a20d98e3fa34508
dz1mka <dzimka@gmail.com>
2025-02-24 01:41:03 +0200

    1 -Scrolling the Environment

---
+++ Assets/GenerateWorld.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateWorld : MonoBehaviour
+{
+    GameObject dummyTraveller;
+
+    void Start()
+    {
+        dummyTraveller = new GameObject("dummy");
+        for (int i = 0; i < 20; i++)
+        {
+            GameObject p = Pool.singleton.GetRandom();
+            if (p == null) return;
+
+            p.SetActive(true);
+            p.transform.position = dummyTraveller.transform.position;
+            p.transform.rotation = dummyTraveller.transform.rotation;
+
+
+            if (p.tag == "stairsUp")
+            {
+                dummyTraveller.transform.Translate(0, 5, 0);
+            }
+            else if (p.tag == "stairsDown")
+            {
+                dummyTraveller.transform.Translate(0, -5, 0);
+                p.transform.Rotate(new Vector3(0, 180, 0));
+                p.transform.position = dummyTraveller.transform.position;
+
+            }
+            else if (p.tag == "platformTSection")
+            {
+                if(Random.Range(0,2) == 0)
+                    dummyTraveller.transform.Rotate(new Vector3(0, 90, 0));
+                else
+                    dummyTraveller.transform.Rotate(new Vector3(0, -90, 0));
+
+
+                dummyTraveller.transform.Translate(Vector3.forward * -10);
+            }
+
+            dummyTraveller.transform.Translate(Vector3.forward * -10);
+
+        }
+    }
+
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Animator anim;
+    public static GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = this.GetComponent<Animator>();
+        player = this.gameObject;
+    }
+
+    void StopJump()
+    {
+        anim.SetBool("isJumping", false);
+    }
+
+    void StopMagic()
+    {
+        anim.SetBool("isMagic", false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && anim.GetBool("isMagic") == false)
+        {
+            anim.SetBool("isJumping", true);
+        }
+        else if (Input.GetKeyDown(KeyCode.M) && anim.GetBool("isJumping") == false)
+        {
+            anim.SetBool("isMagic", true);
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            this.transform.Rotate(Vector3.up * 90);
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            this.transform.Rotate(Vector3.up * -90);
+        }
+        else if (Input.GetKeyDown(KeyCode.A))
+        {
+            this.transform.Translate(-0.1f, 0, 0);
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            this.transform.Translate(0.1f, 0, 0);
+        }
+    }
+}
