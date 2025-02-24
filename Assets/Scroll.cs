698408a85ceb7005cb1460af679cb598bb7dbfc5
dz1mka <dzimka@gmail.com>
2025-02-24 21:10:50 +0200

    2 -Scrolling in the Y

--- Assets/Scroll.cs
+++ Assets/Scroll.cs
@@ -7,5 +7,11 @@ public class Scroll : MonoBehaviour
     private void FixedUpdate()
     {
         this.transform.position += PlayerController.player.transform.forward * -0.1f;
+
+        if (PlayerController.currentPlatform == null) return;
+        if (PlayerController.currentPlatform.tag == "stairsUp")
+            this.transform.Translate(0, -0.06f, 0);
+        if (PlayerController.currentPlatform.tag == "stairsDown")
+            this.transform.Translate(0, 0.06f, 0);
     }
 }
